476e70c2e34033cc5ddcc4e2a72429f99d9a02c1
Michael Stone <mikepwns37@gmail.com>
2019-01-27 15:40:57 -0500

    Add printInitials.cpp file

---
+++ printInitials.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Simple program prints out my initials in block letters
+int main() {
+	std::cout << "M   M   SSS \n";
+	std::cout << "MM MM  S   S\n";
+  	std::cout << "MM MM  S    \n";
+  	std::cout << "M M M   SSS \n";
+  	std::cout << "M   M      S\n";
+  	std::cout << "M   M  S   S\n";
+  	std::cout << "M   M   SSS \n";
+}
